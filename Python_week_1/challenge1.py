f9ccb28f8c9a69607dffa2dd54e2f8d904eaca8a
ahmednule <nuleahmed6@gmail.com>
2024-08-23 23:11:56 +0300

    created a foledr for week 1 python and didi the weekly code challnge, pretty easy, I'm loving python already

---
+++ Python_week_1/challenge1.py
@@ -0,0 +1,8 @@
+# Task: Write a program that asks the user for their name, age, and location and then prints out a personalized message.
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+#  "Hello [name], you are [age] years old and live in [location]."
+print(f"Hello {name}, you are {age} years old and live in {location}.")
---
+++ Python_week_1/index.py
@@ -0,0 +1,30 @@
+# print("Mohammed")
+# print("Daisy is unwell")
+
+# name = "Juiet"
+# print(name)
+
+# This is a comment
+
+# lists
+list = [1, 2, 3 , 4, 5] # has square brackets
+print(list[2])
+list[4] = 20
+print(list)
+
+# tuples
+tuple = (1, 2, 3, 4, 5) # has round brackets
+# tuple[4] = 20
+print(tuple)
+
+students = {
+    "name": "Juliet",
+    "age": 20,
+    "course": "Computer Science"  
+}
+print(students["age"])
+
+# inputing values or user inputes
+
+name = input("Enter your name:  ")
+print("Your name is ", name)
